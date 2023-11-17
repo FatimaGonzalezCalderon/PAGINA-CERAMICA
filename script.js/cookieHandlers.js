be82e40390f6c11ff7250cc528112b2ac4c09b94
fatima gonzalez calderon <fatimagonzalezcalderon@gmail.com>
2023-11-16 22:14:02 -0300

    avance con la pagina, agregue carpeta css, acomode mi html, agregue otras imagenes, agregue whatsapp, le puse un banner de acepto cookies, agregue javascript comentarios

---
+++ script.js/cookieHandlers.js
@@ -0,0 +1,5 @@
+// Este código establece el tamaño de la fuente del texto animado a 2vw.
+var fontSize = "2vw";
+
+// Este código establece las dimensiones del contenedor del texto animado.
+var containerWidth = "100vw";
